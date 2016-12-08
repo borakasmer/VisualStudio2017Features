4231aa3796acc89f7502833aac51b851771396df
borakasmer <bora@borakasmer.com>
2016-12-09 02:22:26 +0300

    Add project files.

---
+++ SeminerScaleGif/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SeminerScaleGif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.rdGif = new System.Windows.Forms.RadioButton();
+            this.rdImage = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rndWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rndHeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Seçin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 82);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dosyayı Boyutlandır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(319, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(902, 553);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(315, 36);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Genişlik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Yükseklik";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(315, 78);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // rdGif
+            // 
+            this.rdGif.AutoSize = true;
+            this.rdGif.Checked = true;
+            this.rdGif.Location = new System.Drawing.Point(351, 115);
+            this.rdGif.Name = "rdGif";
+            this.rdGif.Size = new System.Drawing.Size(38, 17);
+            this.rdGif.TabIndex = 7;
+            this.rdGif.TabStop = true;
+            this.rdGif.Text = "Gif";
+            this.rdGif.UseVisualStyleBackColor = true;
+            // 
+            // rdImage
+            // 
+            this.rdImage.AutoSize = true;
+            this.rdImage.Location = new System.Drawing.Point(451, 115);
+            this.rdImage.Name = "rdImage";
+            this.rdImage.Size = new System.Drawing.Size(54, 17);
+            this.rdImage.TabIndex = 8;
+            this.rdImage.Text = "Resim";
+            this.rdImage.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(644, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 74);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Otomatik Boyut Ver";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rndWidth
+            // 
+            this.rndWidth.Location = new System.Drawing.Point(971, 33);
+            this.rndWidth.Name = "rndWidth";
+            this.rndWidth.Size = new System.Drawing.Size(100, 20);
+            this.rndWidth.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(901, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Genişlik";
+            // 
+            // rndHeight
+            // 
+            this.rndHeight.Location = new System.Drawing.Point(971, 78);
+            this.rndHeight.Name = "rndHeight";
+            this.rndHeight.Size = new System.Drawing.Size(100, 20);
+            this.rndHeight.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(901, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Yükseklik";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 817);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rndHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rndWidth);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.rdImage);
+            this.Controls.Add(this.rdGif);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.RadioButton rdGif;
+        private System.Windows.Forms.RadioButton rdImage;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox rndWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox rndHeight;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SeminerScaleGif/Form1.cs
@@ -0,0 +1,214 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeminerScaleGif
+{
+    public partial class Form1 : Form
+    {
+        public string fileName { get; set; }
+        public String[] SizeArray { get; set; } = new String[] { "100x100", "200x200", "300x300", "400x400", "500x500" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Gif files (*.gif)|*.gif|Png Files (*.png)|*.png";
+            openFileDialog1.InitialDirectory = @"c:\gif2";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    fileName = openFileDialog1.FileName;
+                    pictureBox1.Load(fileName);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int.TryParse(txtWidth.Text, out int widthInt);
+            int.TryParse(txtHeight.Text, out int heightInt);
+            IFType type;
+            if (rdGif.Checked)
+            {
+                type = new Gif() { Width = widthInt, Height = heightInt, GifName = "" };
+            }
+            else
+            {
+                type = new Image() { Width = widthInt, Height = heightInt, ImageName = "" };
+            }
+            //var result = ScaleImage(type);
+            try
+            {
+                (var boolResult, string flName) = ScaleImage(type);
+
+                //(bool boolResult,string fileName) ScaleImage(IFType fileType)
+                (bool boolResult, string flName) ScaleImage(IFType fileType)
+                {
+                    try
+                    {
+                        throw new DivideByZeroException();
+                        switch (fileType)
+                        {
+                            case Gif t:
+                                {
+                                    using (MagickImageCollection collection = new MagickImageCollection(fileName))
+                                    {
+                                        collection.Coalesce();
+                                        MagickGeometry size = new MagickGeometry(t.Width, t.Height);
+                                        size.IgnoreAspectRatio = false;
+
+                                        foreach (MagickImage image in collection)
+                                        {
+                                            image.Resize(size);
+                                        }
+                                        //string folderName = Path.GetFileName(Path.GetDirectoryName(fileName));
+                                        //string gifName = folderName + "\\" + Path.GetFileNameWithoutExtension(fileName) + t.Width + "x" + t.Height + ".gif";
+                                        string gifName = GetFileName(t);
+                                        collection.Write(gifName);
+
+                                        return (true, gifName);
+                                    }
+                                    break;
+                                }
+
+                            case Image t:
+                                {
+                                    using (MagickImage image = new MagickImage(fileName))
+                                    {
+                                        MagickGeometry size = new MagickGeometry(t.Width, t.Height);
+                                        size.IgnoreAspectRatio = false;
+                                        image.Resize(size);
+                                        string imageName = GetFileName(t);
+                                        image.Write(imageName);
+
+                                        return (true, imageName);
+                                        break;
+                                    }
+                                }
+                        }
+
+                        return (true, "");
+                    }
+                    catch
+                    {
+                        return (false, "");
+                    }
+                    return (false, "");
+                }
+
+                string GetFileName(IFType TypeFil)
+                {
+                    switch (TypeFil)
+                    {
+                        case Gif t:
+                            {
+                                //string folderName = Path.GetFileName(Path.GetDirectoryName(fileName));
+                                //string gifName = folderName + "\\" + Path.GetFileNameWithoutExtension(fileName) + t.Width + "x" + t.Height + ".gif";
+                                string gifName = "c:\\Gif2\\" + Path.GetFileNameWithoutExtension(fileName) + t.Width + "x" + t.Height + ".gif";
+                                return gifName;
+                                break;
+                            }
+                        case Image t:
+                            {
+                                //string folderName = Path.GetFileName(Path.GetDirectoryName(fileName));
+                                //string imageName = folderName + "\\" + Path.GetFileNameWithoutExtension(fileName) + t.Width + "x" + t.Height + ".png";
+                                string imageName = "c:\\Gif2\\" + Path.GetFileNameWithoutExtension(fileName) + t.Width + "x" + t.Height + ".png";
+                                return imageName;
+                                break;
+                            }
+                    }
+                    return "";
+                }
+
+                //pictureBox1.Load(result.fileName);
+                pictureBox1.Load(flName);
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int rowNum = rnd.Next(1, 5);
+            ref string size = ref GetRandomSize(rowNum);
+            rndWidth.Text = size.Split('x')[0];
+            rndHeight.Text = size.Split('x')[1];
+
+            size = "121x17";
+
+            MessageBox.Show(SizeArray[rowNum]);
+
+            ref string GetRandomSize(int rowNumber)
+            {
+                return ref SizeArray[rowNumber];
+            }
+
+            /*
+            private void button3Impossible_Click(object sender, EventArgs e)
+            {
+                Random rnd = new Random();
+                int rowNum = rnd.Next(1, 5);
+                //(ref string size,int width,int height) = (ref,,) GetRandomSizeImpossible(rowNum);
+                (ref string resize, int width, int height) = ref GetRandomSizeImpossible(rowNum);
+
+                resize = rndWidth.Text+"x" + rndHeight.Text;
+                MessageBox.Show(SizeArray[rowNum]);               
+
+                (ref string, int width, int height) GetRandomSizeImpossible(int rowNumber)
+                {
+                    int.TryParse(SizeArray[rowNumber].Split('x')[0], out int w);
+                    int.TryParse(SizeArray[rowNumber].Split('x')[1], out int h);
+
+                    return (ref SizeArray[rowNumber], w, h);
+            }
+            */
+        }
+    }
+}
+
+interface IFType
+{
+    int Width { get; set; }
+    int Height { get; set; }
+}
+
+public class Image : IFType
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public string ImageName { get; set; }
+}
+
+public class Gif : IFType
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public string GifName { get; set; }
+}
